735c16015d281851fae8b116df53e3cf655f13aa
quocle <quoc.le@futa.vn>
2025-05-20 11:14:58 +0700

    #8 race exemple 01

---
+++ racy/go.mod
@@ -0,0 +1,3 @@
+module racy
+
+go 1.24.1
---
+++ timer-reset/race.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	var t *time.Timer
+	reset := make(chan bool)
+
+	t = time.AfterFunc(randomDuration(), func() {
+		fmt.Println(time.Now().Sub(start))
+		reset <- true
+	})
+
+	for time.Since(start) < 5*time.Second {
+		<-reset
+		t.Reset(randomDuration())
+	}
+}
+
+func randomDuration() time.Duration {
+	return time.Duration(rand.Int63n(1e9))
+}
